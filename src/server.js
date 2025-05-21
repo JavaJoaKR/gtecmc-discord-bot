394aded57f1a085a7b55a27202d40b853956654e
JavaJoaKR <javapsgkr@gmail.com>
2025-05-21 11:33:32 +0900

    오류 메세지 생략 제거

--- src/server.js
+++ src/server.js
@@ -402,7 +402,7 @@ router.post('/', async (request, env) => {
               return new JsonResponse({
                 type: InteractionResponseType.CHANNEL_MESSAGE_WITH_SOURCE,
                 data: {
-                  content: `**역할 부여 실패!**\n\n인증은 완료되었으나, 역할 부여에 실패했습니다. 봇의 권한을 확인하거나 관리자에게 문의해주세요.\n(Discord API 오류: ${addRoleResponse.status} ${errorText.substring(0, 100)}...)`,
+                  content: `**역할 부여 실패!**\n\n인증은 완료되었으나, 역할 부여에 실패했습니다. 봇의 권한을 확인하거나 관리자에게 문의해주세요.\n(Discord API 오류: ${addRoleResponse.status} ${errorText}...)`,
                   flags: 64,
                 },
               });
